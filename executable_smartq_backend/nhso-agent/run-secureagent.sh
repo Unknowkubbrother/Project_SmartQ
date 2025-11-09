e06a4986d9b718ef7d77c5cea41c75e5e5f931c6
Unknowkubbrother <natchnon654321@gmail.com>
2025-11-09 13:42:50 +0700

    release server

---
+++ executable_smartq_backend/nhso-agent/run-secureagent.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# เปลี่ยน path ให้ตรงกับไฟล์ jar ของคุณ
+JAR_PATH="./secureagent-1.1.2.jar"
+
+# รัน Java ด้วย spring profile test และเปิด module java.prefs
+java --add-opens=java.prefs/java.util.prefs=ALL-UNNAMED \
+     -Dspring.profiles.active=test \
+     -jar "$JAR_PATH"
+
