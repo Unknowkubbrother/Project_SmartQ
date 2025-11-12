1de9ff19d71496c82452eb2cf1d8f7c07f56d30c
Unknowkubbrother <natchnon654321@gmail.com>
2025-11-12 14:08:26 +0700

    update

--- Release/Windows/Server/nhso-agent/run-secureagent_windows.bat
+++ Release/Windows/Server/nhso-agent/run-secureagent_windows.bat
@@ -2,9 +2,9 @@
 
 set JAR_PATH=%~dp0secureagent-1.1.2.jar
 
-REM รัน Java ด้วย spring profile test และเปิด module java.prefs
+REM
 java --add-opens=java.prefs/java.util.prefs=ALL-UNNAMED ^
-     -Dspring.profiles.active=test ^
+     -Dspring.profiles.active=prod ^
      -jar "%JAR_PATH%"
 
 pause
